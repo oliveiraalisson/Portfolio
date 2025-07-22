6dfcc958985d7efcfcf68f1f05c361b9249e9ced
Alisson Oliveira <56358160+oliveiraalisson@users.noreply.github.com>
2025-07-22 11:12:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/alissonoliveira_portfolio/',
   server: {
     host: "::",
     port: 8080,
